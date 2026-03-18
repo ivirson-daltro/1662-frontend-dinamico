07acb61af65aa6caeaf0f6c886fe524732d60c44
Ivirson Daltro <ivi.daltro@gmail.com>
2026-03-18 19:53:58 -0300

    Aula 03 - resolução do exercício de formulario

--- Aula 02/formulario/scripts.js
+++ Aula 02/formulario/scripts.js
@@ -1,12 +1,68 @@
 const form = document.querySelector("#cadastro");
-const usuario = document.querySelector("#usuario").value;
-const senha = document.querySelector("#senha").value;
-const email = document.querySelector("#email").value;
+const usuario = document.querySelector("#usuario");
+const senha = document.querySelector("#senha");
+const email = document.querySelector("#email");
+const error = document.querySelector("#error");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  if (usuario === "" || senha === "" || email === "") {
+  if (usuario.value === "" || senha.value === "" || email.value === "") {
     alert("Alguns dos campos obrigatórios não foram preenchidos!");
+    return;
+  }
+
+  // Validar email no submit
+  if (!email.value.includes("@")) {
+    alert("Digite um email válido");
+  }
+  // let emailPossuiArroba = false;
+  // const charArr = email.value.split("");
+  // charArr.forEach((char) => {
+  //   console.log(char);
+
+  //   if (char === "@") {
+  //     emailPossuiArroba = true;
+  //   }
+  // });
+
+  // if (!emailPossuiArroba) {
+  //   alert("Digite um email válido");
+  // }
+
+  console.log({
+    usuario: usuario.value,
+    email: email.value,
+    senha: senha.value,
+  });
+
+  usuario.value = "";
+  email.value = "";
+  senha.value = "";
+});
+
+// Validar o email durante a digitação
+// email.addEventListener("keyup", () => {
+//   if (!email.value.includes("@")) {
+//     error.innerText = "Um email válido precisa ser inserido";
+//   } else {
+//     error.innerText = "";
+//   }
+// });
+
+// Validar ao sair do campo
+email.addEventListener("blur", () => {
+  if (!email.value.includes("@")) {
+    error.innerText = "Um email válido precisa ser inserido";
+  } else {
+    error.innerText = "";
+  }
+});
+
+senha.addEventListener("blur", () => {
+  if (senha.value.length < 6) {
+    error.innerText = "A senha precisa ter ao menos 6 caracteres";
+  } else {
+    error.innerText = "";
   }
 });
